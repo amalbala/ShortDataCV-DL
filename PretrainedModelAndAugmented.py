3cb0f34261306633ae9dfd9e83426a0f2dd67ea2
antonio <amalbala@gmail.com>
2019-06-13 16:15:38 +0200

    Add Pretrained models

---
+++ PretrainedModelAndAugmented.py
@@ -0,0 +1,89 @@
+import os, shutil
+import numpy as np
+from keras.applications import VGG16
+from keras import layers
+from keras import models
+from keras import optimizers
+from keras.preprocessing.image import ImageDataGenerator
+import matplotlib.pyplot as plt
+
+original_dataset_dir = '/media/antonio/A8D81C3AD81C0968/Data/CatsAndDogs/train'
+base_dir = '/media/antonio/A8D81C3AD81C0968/Data/CatsAndDogs/small'
+train_dir = os.path.join(base_dir, 'train')
+validation_dir= os.path.join(base_dir, 'validation')
+test_dir= os.path.join(base_dir, 'test')
+
+conv_base = VGG16(weights='imagenet',
+                  include_top = False,
+                  input_shape=(150, 150, 3))
+
+conv_base.trainable = False
+
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+train_datagen = ImageDataGenerator(
+    rescale=1./255,
+    rotation_range=40,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True,
+    fill_mode='nearest'
+)
+
+train_generator = train_datagen.flow_from_directory(
+    train_dir,
+    target_size=(150, 150),
+    batch_size=20,
+    class_mode='binary'
+)
+
+validation_generator = test_datagen.flow_from_directory(
+    validation_dir,
+    target_size=(150, 150),
+    batch_size=20,
+    class_mode='binary'
+)
+
+model = models.Sequential()
+
+model.add(conv_base)
+model.add(layers.Flatten())
+model.add(layers.Dense(256, activation='relu'))
+model.add(layers.Dense(1, activation='sigmoid'))
+
+model.compile(
+    optimizer=optimizers.RMSprop(lr=2e-5),
+    loss='binary_crossentropy',
+    metrics=['acc']
+)
+
+history = model.fit_generator(
+    train_generator,
+    steps_per_epoch=100,
+    epochs=30,
+    validation_data=validation_generator,
+    validation_steps=50
+)
+
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs = range(1, len(acc)+1)
+
+plt.plot(epochs, acc, 'bo', label='Training acc')
+plt.plot(epochs, val_acc, 'b', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.legend()
+
+plt.figure()
+
+plt.plot(epochs, loss, 'bo', label='Training loss')
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.legend()
+
+plt.show
